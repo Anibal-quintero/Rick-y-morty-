398ca3eaefddfb146cec356f47f3820787661d9d
Anibal-quintero <amezaquintero5@gmail.com>
2024-04-26 16:14:52 -0500

    Ajustes

--- rick/server/main.server.mjs
+++ rick/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-P37NOFJV.mjs";import"./chunk-NJTE2BND.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LZS6ZDIH.mjs";import"./chunk-NJTE2BND.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
