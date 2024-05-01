6c65e52770347b4e9e599e36a6238ee0206b3e6e
Anibal-quintero <amezaquintero5@gmail.com>
2024-05-01 14:18:24 -0500

    add-loading-state

--- rick/server/main.server.mjs
+++ rick/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-C3JJ6RKD.mjs";import"./chunk-NJTE2BND.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BPE5PKSO.mjs";import"./chunk-NJTE2BND.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
