758ca1f84b306182fe7f46730ee0307b07b050f7
Anibal-quintero <amezaquintero5@gmail.com>
2024-05-06 14:26:36 -0500

    add-component

--- rick/server/main.server.mjs
+++ rick/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-53MMTJUD.mjs";import"./chunk-5ZAUVPJR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-5HQ2HGLO.mjs";import"./chunk-5ZAUVPJR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
